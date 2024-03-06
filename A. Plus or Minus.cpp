1a7f511ad0eb3ee113757bc29bcf9dfe2f93b757
Sabbir  Shihab  JIm <137509544+Sabbir-Shihab@users.noreply.github.com>
2024-03-06 22:38:27 +0600

    Add files via upload

---
+++ A. Plus or Minus.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define fs(lcv,hi) for(int lcv=0;lcv<hi;lcv++)
+#define fb(lcv,hi) for(int lcv=hi; lcv>=0;lcv--)
+using namespace std;
+
+void Tests()
+{
+//code here
+int a,b,c;
+cin>>a>>b>>c;
+if(a+b==c )
+{
+    cout<<"+"<<endl;
+}
+else
+    cout<<"-"<<endl;
+
+
+
+}
+
+int main() {
+
+ios::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+
+ int t;
+ cin>>t;
+ while(t--)
+{
+  Tests();
+ }
+
+return 0;
+}
+
